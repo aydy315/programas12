f7873f5de39d5f5aa92831d94fd9cda2c96dbc28
aydy315 <vallejosllantoya@gmail.com>
2026-04-27 16:57:02 -0500

    Add files via upload

--- ParImpar.java
+++ ParImpar.java
@@ -1,6 +1,6 @@
 public class ParImpar {
     public static void main(String[] args) {
-        int numero = 17;
+        int numero = 24;
         if (numero % 2 == 0) {
         System.out.println(numero + " es par");
         } else {
